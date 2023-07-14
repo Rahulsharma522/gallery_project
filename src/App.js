48b12959a513d3ba129f632711bac59eabe7e541
rahul sharma <sharma.sharma.rahul46@gmail.com>
2023-07-14 10:51:20 +0530

    add project

--- src/App.js
+++ src/App.js
@@ -1,24 +1,59 @@
-import logo from './logo.svg';
 import './App.css';
+import restaurants from "./data"
+import ItemGroup from "./ItemGroup"
+import { useState } from 'react';
 
 function App() {
+  console.log("restauranst", restaurants)
+  const [persons, setPersons] = useState(2)
+  const [total, setTotal] = useState(200)
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main class="mt-10 mx-auto max-w-7xl px-4 sm:mt-12 sm:px-6 md:mt-16 lg:mt-20 lg:px-8 xl:mt-28 flex justify-center flex-col">
+      <div class="sm:text-center lg:text-center">
+        <h1 class="text-4xl tracking-tight font-extrabold text-gray-900 sm:text-5xl md:text-6xl">
+          <span class="block xl:inline">Appetite</span>
+        </h1>
+        <div class="mt-5 sm:mt-8 sm:flex sm:justify-center lg:justify-center">
+          <div class="rounded-md shadow">
+            <div>
+              <div className="mt-1">
+                <span>Persons</span>
+                <input
+                  type="text"
+                  name="persons"
+                  id="persons"
+                  className="px-8 mb-4 border py-3 shadow-sm focus:ring-indigo-500 focus:border-indigo-500 block w-full sm:text-sm border-gray-300 rounded-md"
+                  placeholder="1"
+                  onChange={(e) => {
+                    console.log(e.target.value,
+                      "e")
+                    setPersons(e.target.value)
+                  }}
+                  defaultValue={persons}
+                />
+                <span>Budget</span>
+                <input
+                  type="number"
+                  name="number"
+                  id="number"
+                  className="px-8 mb-4 border py-3 shadow-sm focus:ring-indigo-500 focus:border-indigo-500 block w-full sm:text-sm border-gray-300 rounded-md"
+                  placeholder="500"
+                  onChange={(e) => {
+                    console.log(e.target.value,
+                      "e")
+                    setTotal(e.target.value)
+                  }}
+                  defaultValue={total}
+                />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <ItemGroup groups={restaurants} restaurants={restaurants} persons={persons} total={total} />
+    </main>
   );
 }
 
---
+++ src/ItemGroup.js
@@ -0,0 +1,59 @@
+import { combinations, filteredCombinations, totalByGroup } from "./utils";
+const ItemGroup = (props) => {
+  const Items = (groups, persons) => {
+    const MenuItems = (items, index) => {
+      return items.map((item, index) => {
+        return (
+          <div
+            key={index}
+            className="bg-gray-50 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6"
+          >
+            <dt className="text-sm font-medium text-gray-500">{item.name}</dt>
+            <dd className="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
+              X {persons}
+            </dd>
+          </div>
+        );
+      });
+    };
+    
+    return groups.map((group, index) => {
+      return (
+        <div
+          key={index}
+          className="bg-white shadow overflow-hidden sm:rounded-lg"
+        >
+          <div className="px-4 py-5 sm:px-6">
+            <h3 className="text-lg leading-6 font-medium text-gray-900">
+              Group {index + 1}
+            </h3>
+          </div>
+
+          <div className="border-t border-gray-200">
+            <dl>{MenuItems(group)}</dl>
+          </div>
+        </div>
+      );
+    });
+  };
+
+   const grouped = totalByGroup(
+    props.restaurants,
+    props.persons,
+    props.total
+  ).filter((r) => r.groups.length > 0);
+
+  return grouped.map((res, i) => (
+    <div>
+      <h3 className="text-3xl py-8 leading-6 font-medium text-gray-900">
+        {res.shopName}
+      </h3>
+     
+      <div className="md:grid md:grid-cols-4 gap-4">
+        {Items(res.groups, props.persons)}
+      </div>
+    </div>
+  ));
+};
+
+export default ItemGroup;
